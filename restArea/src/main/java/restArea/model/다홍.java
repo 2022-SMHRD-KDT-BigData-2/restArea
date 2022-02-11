1429353cb16c07af741c0ba995d6fd0467b0c11d
smhrd <smhrd@59.3.58.116>
2022-02-11 11:35:37 +0900

    신다홍

---
+++ restArea/src/main/java/restArea/model/다홍.java
@@ -0,0 +1,24 @@
+package restArea.model;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class dahong
+ */
+@WebServlet("/dahong")
+public class ´ÙÈ« extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
